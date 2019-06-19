a27a5cd0498d0f181747efe176d62286d73ac357
Johannes Starke <starke@develappers.de>
2019-06-19 10:53:59 +0200

    Added test action

---
+++ lib/fastlane/plugin/develappers/actions/xcode_test_action.rb
@@ -0,0 +1,63 @@
+module Fastlane
+    module Actions
+        class XcodeTestAction < Action
+            def self.run(params)
+                ### handle params
+                
+                # version
+                xcode_version = params[:version]
+                
+                ### build
+                # pod install
+                UI.important "Pod install"
+                other_action.cocoapods(repo_update: false)
+
+                # set xcode version
+                unless xcode_version.nil?
+                    UI.important "Set xcode version to #{xcode_version}"
+                    xcode_path = other_action.xcode_install(version: xcode_version)
+                    other_action.xcode_select(xcode_path)
+                end
+
+                # test application
+                UI.important "Test application"
+
+                # detect workspace and use absolute path
+                empty_config = {}
+                FastlaneCore::Project.detect_projects(empty_config)
+                workspace = empty_config[:workspace]
+                workspace = File.realdirpath(workspace)
+
+                other_action.scan(workspace: workspace)
+            rescue Exception => e
+                # reraise
+                UI.abort_with_message! e.message
+            end
+
+            def self.description
+                "Tests a iOS project"
+            end
+
+            def self.authors
+                ["Johannes Starke"]
+            end
+
+            def self.return_value
+                nil
+            end
+
+            def self.details
+            end
+
+            def self.available_options
+                [
+                    FastlaneCore::ConfigItem.new(key: :version, env_name: "BUILD_XCODE_VERSION", description: "Xcode version (e.g. 9.1, 9.2)", type: String, optional: true)
+                ]
+            end
+
+            def self.is_supported?(platform)
+                [:ios].include?(platform)
+            end
+        end
+    end
+  end
